62b5ce4e6800320989f32b837c69b1a7e3074e57
itswagi <43064115+itswagi@users.noreply.github.com>
2020-10-30 02:15:10 +0500

    Added saved code

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+  constructor(board){
+    this._board = board
+  }
+  print(){
+    console.log(this._board.join('\n'))
+  }
+}
+
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
+
+myField.print()
